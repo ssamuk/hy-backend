7fce02a0dff332ed0c08c952645f86874611ffc1
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2020-11-04 16:02:53 +0200

    3.3 done

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
   
-  if (note) {
+  if (person) {
     response.json(person)
   } else {
     response.status(404).end()
