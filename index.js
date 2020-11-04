fe6b8966d368b6c84658a7d51038d0e6103a9dc8
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2020-11-04 15:42:11 +0200

    3.2 done

--- index.js
+++ index.js
@@ -3,58 +3,76 @@ const app = express()
 
 app.use(express.json()) 
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2020-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2020-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2020-01-10T19:20:14.298Z",
-    important: true
-  }
+let persons = [
+    
+      { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+        
+      },
+      { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+        
+      },
+      { 
+
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+      },
+      { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+        
+      }
+    
+  
 ]
 
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
+
+
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
 })
 
+app.get('/api/persons/info', (request, response) => {
+      response.write(`<p>Phonebook has info for ${persons.length} people</p>`)
+      response.write(`<p>${new Date()}</p>`)
+      response.end()
+    }
+)
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+    const person = persons.find(person => person.id === id)
   
   if (note) {
-    response.json(note)
+    response.json(person)
   } else {
     response.status(404).end()
   }
 })
  
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+    perosns = persons.filter(person => person.id !== id)
   
     response.status(204).end()
 })
  
 const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
       : 0
     return maxId + 1
 }
   
-app.post('/api/notes', (request, response) => {
+/*app.post('/api/persons', (request, response) => {
     const body = request.body
   
     if (!body.content) {
@@ -63,17 +81,17 @@ app.post('/api/notes', (request, response) => {
       })
     }
   
-    const note = {
+    const person = {
       content: body.content,
-      important: body.important || false,
-      date: new Date(),
+      //important: body.important || false,
+      //date: new Date(),
       id: generateId(),
     }
   
     notes = notes.concat(note)
   
     response.json(note)
-})
+})*/
  
  
  
