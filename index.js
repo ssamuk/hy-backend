6353f8e04bca90e36ae2759d83f3b17f57fe60fa
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2020-11-11 23:42:11 +0200

    uibuild

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(requestLogger)
-/*
+
 let persons = [
     
       { 
@@ -73,7 +73,7 @@ let persons = [
     
   
 ]
-*/
+
 
 
 app.get('/api/persons', (request, response) => {
