5fe159d9b752f3320b5e81b3ba5d5a6249b38440
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2020-11-11 12:47:08 +0200

    Added build

---
+++ build/precache-manifest.ab4fd74bb0125e370f15cb3172529e38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d544cac5078d8be31db53cc2ace51e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "266515a7066015795ff6",
+    "url": "/static/css/main.48721ff5.chunk.css"
+  },
+  {
+    "revision": "d982e405c25a88eaf9b0",
+    "url": "/static/js/2.541a58d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.541a58d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266515a7066015795ff6",
+    "url": "/static/js/main.cae45040.chunk.js"
+  },
+  {
+    "revision": "a3d2bf21716328fc74a3",
+    "url": "/static/js/runtime-main.d8499093.js"
+  }
+]);
