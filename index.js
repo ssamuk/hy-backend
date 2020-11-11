35daea9d5ed60f32fa1ff3c37cbce127cf4e9efa
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2020-11-11 23:37:11 +0200

    uibuild

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { token } = require('morgan')
 var morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 app.use(express.static('build'))
 app.use(cors())
@@ -18,13 +19,25 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.ackfw.mongodb.net/persons-app?retryWrites=true&w=majority`
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: String
+})
+
+const Person = mongoose.model('Person', personSchema)
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(requestLogger)
-
+/*
 let persons = [
     
       { 
@@ -60,12 +73,13 @@ let persons = [
     
   
 ]
-
-
+*/
 
 
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
     response.json(persons)
+  })
 })
 
 app.get('/api/persons/info', (request, response) => {
--- mongo.js
+++ mongo.js
@@ -1,3 +1,4 @@
+const { request, response } = require('express')
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
@@ -11,12 +12,13 @@ const url = `mongodb+srv://fullstack:${password}@cluster0.ackfw.mongodb.net/pers
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
-const noteSchema = new mongoose.Schema({
+const personSchema = new mongoose.Schema({
   name: String,
   number: String
 })
 
-const Person = mongoose.model('Person', noteSchema)
+const Person = mongoose.model('Person', personSchema)
+
 
 if (process.argv.length > 4) {
     const person = new Person({
@@ -30,11 +32,13 @@ if (process.argv.length > 4) {
         'to phonebook.')
         mongoose.connection.close()
       })
-/*const person = new Person({
+/*
+const person = new Person({
     name: `Samu`,
     number: `0400123123`,
     id: 1
-})*/
+})
+*/
 }else{
 
 Person.find({}).then(person => {
