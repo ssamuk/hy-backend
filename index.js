a1a55d60dd077b238e912fa61a4c677488ba4b70
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2020-11-13 23:20:23 +0200

    --

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ app.post('/api/persons', (request, response) => {
  
  app.use(unknownEndpoint)
  
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
