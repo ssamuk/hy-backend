e9749f3a139ce3352001cb443838042b07539e6e
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2020-11-17 23:40:58 +0200

    uibuild

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d1e6400e687cbaa7baf675a72f093789.js"
+  "/precache-manifest.a10603bb60dcb1762e5efd17768a94de.js"
 );
 
 self.addEventListener('message', (event) => {
