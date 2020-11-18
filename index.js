47f956538b9e5d321a007edf13a7976d456253d7
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2020-11-18 14:30:00 +0200

    tasks till 3.16 might be ready, going to make sure later.

--- index.js
+++ index.js
@@ -66,11 +66,12 @@ let persons = [
 
 
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({})
     .then((people) => {
       response.json(people)
     })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/info', (request, response) => {
@@ -80,23 +81,24 @@ app.get('/api/persons/info', (request, response) => {
     }
 )
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
   })
-  
-  /*if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }*/
+  .catch(error => next(error))
 })
  
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
  
 /*const generateId = () => {
@@ -106,7 +108,7 @@ app.delete('/api/persons/:id', (request, response) => {
     return maxId + 1
 }*/
   
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     //console.log(persons)
     //console.log(body)
@@ -122,12 +124,32 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
  
+
+
+// Olemattomat osoitteet
+//Nämä on tärkeä olla vasta lopussa.
  app.use(unknownEndpoint)
  
+ const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+
+// Virheelliset pyynnöt
+app.use(errorHandler)
+
 const PORT = process.env.PORT
   app.listen(PORT, () => {
-    console.log('Process.env: ', process.env)
+    //Line below was for debugging
+    // console.log('Process.env: ', process.env)
     console.log(`Server running on port ${PORT}`)
   })
