a8d7f3f9dc308028d02871238abb6476f5d61c61
Samu Kaijansinkko <samu.kaijansinkko@student.lab.fi>
2021-02-15 23:50:45 +0200

    Trying requirements

--- models/person.js
+++ models/person.js
@@ -11,7 +11,11 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   })
 
   const personSchema = new mongoose.Schema({
-    name: String,
+    name: {
+      type: String,
+      minlength: 3,
+      required: true
+    },
     number: String,
     id : String
   })
